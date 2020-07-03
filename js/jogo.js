993629704574cfe71f0170dd587ee3c8711b38a0
Igor Marins <igormarins6@gmail.com>
2020-07-02 23:44:58 -0300

    Refinando alguns componentes em css

--- js/jogo.js
+++ js/jogo.js
@@ -1,7 +1,6 @@
 // Aqui irá rodar a lógica principal do jogo
-while(true){
-    let jogada = prompt('Clique (OK) para continuar jogando ou (Cancelar) para Parar');
-    if(jogada === null){
-        break;
-    }
+while (true) {
+	if (jogada === null) {
+		break;
+	}
 }
